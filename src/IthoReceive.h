14ed4a15dab87ccf1e9abc3b69a1eaecdb2577be
Wim <you@example.com>
2018-04-14 21:53:27 +0200

    moved more code

--- src/IthoReceive.h
+++ src/IthoReceive.h
@@ -1,37 +1,28 @@
 #include <Arduino.h>
 
-class MqttCom;
-class ByteArray;
-class CC1101Packet;
-
-class RfRemoteClass
+class IthoReceiveClass
 {
   public:
-    RfRemoteClass() {}
+	IthoReceiveClass() {}
 
-    void setup();
-    void loop();
+	void setup();
+	void loop();
 
-    void attachIter();
-    void detachIter();
-    void resetBuffer();
+	void attachIter();
+	void detachIter();
+	void resetBuffer();
 
-    bool printAllPacket = false;
-    bool printNonRemote = false;
-    bool printDebug = false;
-
-    void turnOn();
-    void turnOff();
-
-    //void convertToPacket(const ByteArray &, CC1101Packet &);
-    static String toString(uint8_t *data, unsigned int length, bool ashex = true);
+	bool printAllPacket = false;
+	bool printNonRemote = false;
+	bool printDebug = false;
 
   private:
-    bool _gotIthoStart;
-    uint8_t _lastIndex;
+	bool _gotIthoStart;
+	uint8_t _lastIndex;
+	size_t _oldSize = 0;
+	size_t _checkIdx = 0;
 
-    size_t oldSize = 0;
-    size_t checkIdx = 0;
+	static String toString(uint8_t *data, unsigned int length, bool ashex = true);
 };
 
-extern RfRemoteClass RfRemote;
+extern IthoReceiveClass IthoReceive;
